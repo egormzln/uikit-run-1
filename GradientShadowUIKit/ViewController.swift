00cb82460f01d50c6741ada6db3c6cfcdb84a0b9
Egor Mzln <egormizulin@mail.ru>
2024-11-01 22:34:20 +0300

    Initial Commit

---
+++ GradientShadowUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GradientShadowUIKit
+//
+//  Created by Егор Мизюлин on 01.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
